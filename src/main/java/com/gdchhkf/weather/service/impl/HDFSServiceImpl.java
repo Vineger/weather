8617b0da8a09af5d991e17a31a636c79ea314b80
Vineger <gdchhkf@163.com>
2019-03-09 21:37:17 +0800

    fix weather of week bug
    - fix problem in HDFS. The way to build weather's data about the past week

--- src/main/java/com/gdchhkf/weather/service/impl/HDFSServiceImpl.java
+++ src/main/java/com/gdchhkf/weather/service/impl/HDFSServiceImpl.java
@@ -15,17 +15,12 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
- * @ClassName HDFSServiceImpl
- * @Description HDFS操作集合
- * @Author gdchhkf@163.com
- * @Date 2019/2/28 15:05
- * @Version 1.0
+ * HDFS常用的操作集合
+ * @author gdchhkf@163.com
+ * @version 1.0
  **/
 @Service
 public class HDFSServiceImpl implements HDFSService {
@@ -36,19 +31,20 @@ public class HDFSServiceImpl implements HDFSService {
     private TimeUtils utils;
     
     /**
-     * @Author gdchhkf@163.com
-     * @Description
-     * @Date 13:09 2019/3/4
-     * @Param [week]
+     * 获取分析过去一周气象数据文件的结果
+     * @author gdchhkf@163.com
+     * @param week
      * @return void
      **/
     public void setLastWeekResult (WeatherWeek week) {
-        //获得MapReduce 统计出来的数据
+        //组装结果文件路径
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu_mm_dd");
         String fileName = utils.getLastMonday().format(formatter);
         String file = "/weather/week/" + fileName;
+        //调用 readFile() 获取文件中的信息
         String temp = readFile(file);
-        
+
+        //对文件信息进行处理, 封装
         StringTokenizer tokenizer = new StringTokenizer(temp);
         while (tokenizer.hasMoreTokens()) {
             String key = tokenizer.nextToken();
@@ -64,17 +60,16 @@ public class HDFSServiceImpl implements HDFSService {
             }
         }
     }
-    
+
     /**
-     * @Author gdchhkf@163.com
-     * @Description 
-     * @Date 12:35 2019/3/4
-     * @Param []
-     * @return java.util.Map
+     *
+     * @author gdchhkf@163.com
+     * @return void
      **/
-    public Map readLastWeekFiles () {
+    public List<Map> readLastWeekFiles () {
         List<String> files = utils.getExistsPastWeekFile();
-        Map weatherMap = new HashMap();
+        List<Map> weatherList = new ArrayList<>();
+        Map weather = new HashMap();
 
         for (String file : files) {
             String temp = readFile(file);
@@ -82,18 +77,17 @@ public class HDFSServiceImpl implements HDFSService {
             while (tokenizer.hasMoreTokens()) {
                 String key = tokenizer.nextToken();
                 String value = tokenizer.nextToken();
-                weatherMap.put(key, value);
+                weather.put(key, value);
             }
+            weatherList.add(weather);
         }
-        return weatherMap;
+        return weatherList;
     }
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description
-     * @Date 12:35 2019/3/4
-     * @Param [file]
-     * @return boolean
+     * @author gdchhkf@163.com
+     * @param week
+     * @return void
      **/
     public boolean exists(String file) {
         boolean result = false;
@@ -108,11 +102,9 @@ public class HDFSServiceImpl implements HDFSService {
     }
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description
-     * @Date 15:21 2019/3/3
-     * @Param [fileName]
-     * @return java.lang.String
+     * @author gdchhkf@163.com
+     * @param week
+     * @return void
      **/
     public String readFile(String fileName){
         String result = "";
@@ -128,13 +120,9 @@ public class HDFSServiceImpl implements HDFSService {
 
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description
-     *      往HDFS中写文件的辅助方法. 内容写入为追加的形式.
-     *      文件不存在则创建文件.
-     * @Date 16:18 2019/2/28
-     * @Param [filePath, content]
-     * @return boolean
+     * @author gdchhkf@163.com
+     * @param week
+     * @return void
      **/
     public boolean appendFile(String filePath, String content) {
         Path path = new Path(filePath);
@@ -164,11 +152,9 @@ public class HDFSServiceImpl implements HDFSService {
 
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description 移动暂存的气象数据文件到其他位置, 并修改其名字
-     * @Date 15:53 2019/3/2
-     * @Param
-     * @return
+     * @author gdchhkf@163.com
+     * @param week
+     * @return void
      **/
     public boolean renameFile(String srcDir, String desDir, String newFileName){
         boolean result = false;
@@ -184,11 +170,9 @@ public class HDFSServiceImpl implements HDFSService {
 
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description 递归删除HDFS的目录及其文件
-     * @Date 14:19 2019/3/3
-     * @Param [dir]
-     * @return boolean
+     * @author gdchhkf@163.com
+     * @param week
+     * @return void
      **/
     public boolean deleteDir(String dir) {
         boolean result = false;
@@ -201,10 +185,8 @@ public class HDFSServiceImpl implements HDFSService {
     }
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description 关闭FSDataOutputStream的辅助方法
-     * @Date 16:10 2019/2/28
-     * @Param [outputStream]
+     * @author gdchhkf@163.com
+     * @param week
      * @return void
      **/
     private void closeFSDataOutPutStream(FSDataOutputStream outputStream){
@@ -218,10 +200,8 @@ public class HDFSServiceImpl implements HDFSService {
     }
 
     /**
-     * @Author gdchhkf@163.com
-     * @Description 关闭PrintWriter的辅助方法
-     * @Date 16:17 2019/2/28
-     * @Param [writer]
+     * @author gdchhkf@163.com
+     * @param week
      * @return void
      **/
     private void closePrintWriter(PrintWriter writer){
